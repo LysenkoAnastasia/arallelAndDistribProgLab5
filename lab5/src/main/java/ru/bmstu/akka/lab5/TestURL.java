7fe26f0dc7ff43b42bfa27e58063241bb3304fae
LysenkoAnastasia <lysenko2712@mail.ru>
2019-11-22 17:00:44 +0300

    Scripted auto-commit on change (2019-11-22 17:00:43) by gitwatch.sh

--- lab5/src/main/java/ru/bmstu/akka/lab5/TestURL.java
+++ lab5/src/main/java/ru/bmstu/akka/lab5/TestURL.java
@@ -3,5 +3,21 @@ package ru.bmstu.akka.lab5;
 import java.io.Serializable;
 
 public class TestURL implements Serializable {
-    private 
+    private String url;
+    private Integer count;
+
+    public TestURL(String url, Integer count) {
+        this.url = url;
+        this.count = count;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+    
 }
+
