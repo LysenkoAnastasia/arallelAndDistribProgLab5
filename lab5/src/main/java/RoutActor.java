d64c27ab0eec75c1583db42e11213ba947ac4ea9
LysenkoAnastasia <lysenko2712@mail.ru>
2019-11-22 16:22:37 +0300

    Scripted auto-commit on change (2019-11-22 16:22:37) by gitwatch.sh

--- lab5/src/main/java/RoutActor.java
+++ lab5/src/main/java/RoutActor.java
@@ -1,6 +1,8 @@
 import akka.actor.AbstractActor;
 
-public class RoutActor  extends AbstractActor {
+public class RoutActor extends AbstractActor {
+
+
     @Override
     public Receive createReceive() {
         return null;
