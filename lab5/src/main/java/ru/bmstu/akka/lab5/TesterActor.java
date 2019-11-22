105e84f98d61f11e65a904b4ea9e4fcb630264c5
LysenkoAnastasia <lysenko2712@mail.ru>
2019-11-22 16:27:46 +0300

    Scripted auto-commit on change (2019-11-22 16:27:46) by gitwatch.sh

--- lab5/src/main/java/ru/bmstu/akka/lab5/TesterActor.java
+++ lab5/src/main/java/ru/bmstu/akka/lab5/TesterActor.java
@@ -1,16 +1,19 @@
 package ru.bmstu.akka.lab5;
 
 import akka.actor.AbstractActor;
+import akka.japi.pf.ReceiveBuilder;
 
 public class TesterActor extends AbstractActor {
 
 
     public TesterActor() {
     }
-    
+
 
     @Override
     public Receive createReceive() {
-        return null;
+        return ReceiveBuilder.create()
+                .match(TestMsg.class, this::resultTest)
+                .build();
     }
 }
