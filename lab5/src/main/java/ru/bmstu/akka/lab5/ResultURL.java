1464b13917eda49babea043e2ba060a521c36fb5
LysenkoAnastasia <lysenko2712@mail.ru>
2019-11-26 11:35:29 +0300

    Scripted auto-commit on change (2019-11-26 11:35:29) by gitwatch.sh

--- lab5/src/main/java/ru/bmstu/akka/lab5/ResultURL.java
+++ lab5/src/main/java/ru/bmstu/akka/lab5/ResultURL.java
@@ -1,6 +1,21 @@
 package ru.bmstu.akka.lab5;
 
-public class ResultURL {
-    
+import java.io.Serializable;
 
+public class ResultURL implements Serializable {
+    private String result;
+    private Integer count;
+
+    public ResultURL(String result, Integer count) {
+        this.result = result;
+        this.count = count;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
 }
