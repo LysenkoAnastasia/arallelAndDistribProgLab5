fd1e359d55f98487d5abe1fac008bc84b30c7cf3
LysenkoAnastasia <lysenko2712@mail.ru>
2019-11-26 16:14:08 +0300

    Scripted auto-commit on change (2019-11-26 16:14:08) by gitwatch.sh

--- lab5/src/main/java/ru/bmstu/akka/lab5/Tester.java
+++ lab5/src/main/java/ru/bmstu/akka/lab5/Tester.java
@@ -35,6 +35,7 @@ public class Tester {
     private CompletionStage<ResultURL> processTest(TestURL testURL) {
        Patterns.ask(actorRef, testURL, 5000)
                .andThen()
+               .thenCompose()
 
 
 
