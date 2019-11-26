e39ef6fab0fcc4bbdc1f4059276c28fe00bd845e
LysenkoAnastasia <lysenko2712@mail.ru>
2019-11-26 13:14:21 +0300

    Scripted auto-commit on change (2019-11-26 13:14:21) by gitwatch.sh

--- lab5/src/main/java/ru/bmstu/akka/lab5/Main.java
+++ lab5/src/main/java/ru/bmstu/akka/lab5/Main.java
@@ -38,6 +38,7 @@ public class Main {
 
     private Flow<HttpRequest, HttpResponse, NotUsed> createRoute(ActorSystem system, ActorMaterializer materializer) {
         return Flow.of(HttpRequest.class)
+                .map(r -> )
     }
 
 }
