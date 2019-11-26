fa10d635e5182cc9d4e92880a457b413531d2ca8
LysenkoAnastasia <lysenko2712@mail.ru>
2019-11-26 12:47:48 +0300

    Scripted auto-commit on change (2019-11-26 12:47:48) by gitwatch.sh

--- lab5/src/main/java/ru/bmstu/akka/lab5/TestUrlMsg.java
+++ lab5/src/main/java/ru/bmstu/akka/lab5/TestUrlMsg.java
@@ -3,6 +3,10 @@ package ru.bmstu.akka.lab5;
 public class TestUrlMsg {
     private ResultURL resultURL;
 
+    public TestUrlMsg(ResultURL resultURL) {
+        this.resultURL = resultURL;
+    }
+
     public ResultURL getResultURL() {
         return resultURL;
     }
