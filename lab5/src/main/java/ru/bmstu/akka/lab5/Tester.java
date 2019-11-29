876c8c17f3207006200fe275f69e53b932b0764c
LysenkoAnastasia <lysenko2712@mail.ru>
2019-11-29 14:14:23 +0300

    Scripted auto-commit on change (2019-11-29 14:14:23) by gitwatch.sh

--- lab5/src/main/java/ru/bmstu/akka/lab5/Tester.java
+++ lab5/src/main/java/ru/bmstu/akka/lab5/Tester.java
@@ -4,12 +4,15 @@ import akka.actor.ActorSystem;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
 import akka.http.javadsl.model.Query;
+import akka.pattern.Patterns;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
 import ru.bmstu.akka.lab5.ResultURL;
 import ru.bmstu.akka.lab5.TestURL;
+import ru.bmstu.akka.lab5.TestUrlMsg;
 
 import java.util.Optional;
+import java.util.concurrent.CompletionStage;
 
 public class Tester {
     private ActorRef actorRef;
@@ -34,7 +37,7 @@ public class Tester {
        return Patterns.ask(this.actorRef, testURL, 5000)
                .thenApply(r -> (TestUrlMsg)r)
                .thenCompose(res -> {
-                   return res
+                   return res.
                        }
                        )
 
