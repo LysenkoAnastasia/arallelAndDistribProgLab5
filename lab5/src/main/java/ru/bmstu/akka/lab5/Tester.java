e2db47b5fd9460fc79277f38c28af0a298998271
LysenkoAnastasia <lysenko2712@mail.ru>
2019-11-29 18:39:06 +0300

    Scripted auto-commit on change (2019-11-29 18:39:06) by gitwatch.sh

--- lab5/src/main/java/ru/bmstu/akka/lab5/Tester.java
+++ lab5/src/main/java/ru/bmstu/akka/lab5/Tester.java
@@ -57,7 +57,7 @@ public class Tester {
 
     private CompletionStage<ResultURL> startTest(TestURL testURL) {
         Source.from(Collections.singletonList(testURL))
-                .
+                .map()
     }
 
 
