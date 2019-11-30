1ed50010bc35e9b4fed9596f95972be8c0118140
LysenkoAnastasia <lysenko2712@mail.ru>
2019-11-30 22:50:52 +0300

    Scripted auto-commit on change (2019-11-30 22:50:52) by gitwatch.sh

--- lab5/src/main/java/ru/bmstu/akka/lab5/Tester.java
+++ lab5/src/main/java/ru/bmstu/akka/lab5/Tester.java
@@ -95,7 +95,7 @@ public class Tester {
         return  HttpResponse.create()
                 .withStatus(StatusCodes.OK)
                 .withEntity(ContentTypes.APPLICATION_JSON, ByteString.fromString(
-                        Jackson.marshaller(new ObjectMapper().writer().withDefaultPrettyPrinter())
+                        new ObjectMapper().writer().withDefaultPrettyPrinter().writeValueAsString(resultURL)
                 ));
 
     }
