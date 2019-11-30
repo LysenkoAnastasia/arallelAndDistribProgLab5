8f7fc8c383551e66ed206c2fe000a9e24307350a
LysenkoAnastasia <lysenko2712@mail.ru>
2019-11-30 23:10:21 +0300

    Scripted auto-commit on change (2019-11-30 23:10:21) by gitwatch.sh

--- lab5/src/main/java/ru/bmstu/akka/lab5/StoreActor.java
+++ lab5/src/main/java/ru/bmstu/akka/lab5/StoreActor.java
@@ -7,7 +7,7 @@ import java.util.HashMap;
 
 public class StoreActor extends AbstractActor {
 
-    private HashMap<TestURL, Integer> storage;
+    private HashMap<TestURL, Long> storage;
 
 
     public StoreActor() {
